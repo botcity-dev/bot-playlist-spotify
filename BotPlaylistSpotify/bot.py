2a97512f9b893112c72a5c74a6b6b1fa489591d2
morgannadev <morgcontato@gmail.com>
2023-10-17 15:02:37 -0300

    DEV: find music and try to add to the playlist

--- BotPlaylistSpotify/bot.py
+++ BotPlaylistSpotify/bot.py
@@ -4,6 +4,8 @@ from botcity.web import WebBot, Browser, By
 # Import for integration with BotCity Maestro SDK
 from botcity.maestro import *
 
+import os
+
 # Disable errors if we are not connected to Maestro
 BotMaestroSDK.RAISE_NOT_CONNECTED = False
 
@@ -35,10 +37,38 @@ def main():
     # Fazendo login
     entrar = bot.find_element(".LKFFk88SIRC9QKKUWR5u > button:nth-child(2)", By.CSS_SELECTOR)
     entrar.click()
+
+    username = os.getenv("EMAIL_SPOTIFY")
+    password = os.getenv("PASSWORD_SPOTIFY")
     
     login = bot.find_element("login-username", By.ID)
     login.click()
-    login.send_keys("email@email.com")
+    login.send_keys(username)
+    bot.tab()
+    bot.paste(password)
+    bot.enter()
+
+    playlist = bot.find_element(".RowButton-sc-xxkq4e-0", By.CSS_SELECTOR)
+    playlist.click()
+
+    buscar_mais = bot.find_element("div.eoWRdH", By.CSS_SELECTOR)
+    buscar_mais.click()
+
+    alerta = bot.find_element("button.feKImU:nth-child(2)", By.CSS_SELECTOR)
+    alerta.click()    
+
+    campo_buscar_musicas = bot.find_element("input.Type__TypeElement-sc-goli3j-0", By.CSS_SELECTOR)
+    campo_buscar_musicas.click()
+
+    # necessário descer a tela para o botão de adicionar ser encontrado na próxima etapa
+    bot.scroll_down(clicks=200)
+
+    # dica: deixar o mais específico possível (nome da música e banda)
+    campo_buscar_musicas.send_keys("dormi na praça bruno e marrone")
+    
+    botao_adicionar = bot.find_element("/html/body/div[4]/div/div[2]/div[4]/div[1]/div[2]/div[2]/div/div/div[2]/main/div[1]/section/div[2]/div[3]/div[2]/div[1]/div/div[2]/div[1]/div/div[4]/button", By.XPATH)
+    botao_adicionar.click()
+
 
     # Wait 3 seconds before closing
     #bot.wait(3000)
